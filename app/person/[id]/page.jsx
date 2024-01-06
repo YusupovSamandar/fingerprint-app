ac85952df4f587be3e1fdfbfea1aa7db34992615
Samandar Yusupov <yusupovvsamandar@gmail.com>
2024-01-07 00:12:41 +0500

    fix

--- app/person/[id]/page.jsx
+++ app/person/[id]/page.jsx
@@ -1,30 +1,31 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import Table from '@/app/components/personTable';
+"use client";
+import React, { useEffect, useState } from "react";
+import Table from "@/app/components/personTable";
 import Navbar from "@/app/components/navbar";
-import axios from 'axios';
-import { API_URL } from '@/app/apiConfig';
-"august 12"
+import axios from "axios";
+import { API_URL } from "@/app/apiConfig";
+("august 12");
 
-export default function page({ params }) {
-    const fingerID = params.id
-    const [personData, setPersonData] = useState([]);
-    useEffect(() => {
-        (async function () {
-            const { data } = await axios.get(`${API_URL}/api/attendance/staff/${fingerID}`);
-            setPersonData(data);
-        })();
-    }, []);
-    return (
-        <div>
-            <Navbar headingName={"Samandar Yusupov"} />
-            <br />
-            {
-                personData.length > 0 ?
-                    <Table rows={personData} />
-                    : <h4 style={{ textAlign: "center" }} >no data to display</h4>
-            }
-
-        </div>
-    )
+export default function Page({ params }) {
+  const fingerID = params.id;
+  const [personData, setPersonData] = useState([]);
+  useEffect(() => {
+    (async function () {
+      const { data } = await axios.get(
+        `${API_URL}/api/attendance/staff/${fingerID}`
+      );
+      setPersonData(data);
+    })();
+  }, []);
+  return (
+    <div>
+      <Navbar headingName={"Samandar Yusupov"} />
+      <br />
+      {personData.length > 0 ? (
+        <Table rows={personData} />
+      ) : (
+        <h4 style={{ textAlign: "center" }}>no data to display</h4>
+      )}
+    </div>
+  );
 }
